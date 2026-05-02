ea4a0b9780e5e88f20f7fe0e10d47320f03cb30f
Angela <angelastrechen1@mail.com>
2026-05-02 10:56:02 +0200

    Fix smooth scroll and menu overflow behavior

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,16 +7,6 @@ document.addEventListener('DOMContentLoaded', () => {
   const prevBtn = document.querySelector('.js-prev');
   const nextBtn = document.querySelector('.js-next');
 
-  const menu = document.querySelector('.page__menu');
-
-  window.addEventListener('hashchange', () => {
-    if (window.location.hash === '#burger-menu') {
-      document.body.classList.add('menu-open');
-    } else {
-      document.body.classList.remove('menu-open');
-    }
-  });
-
   const slides = [
     {
       image: './src/images/slider/header-photo.jpg',
